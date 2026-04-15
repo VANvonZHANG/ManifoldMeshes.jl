0b03cc5e5983efc3e6db131e2f90f8b5275e14dc
Fan Zhang <vanvonzhang@gmail.com>
2026-04-15 17:33:58 +0800

    init: project skeleton with entry point and test runner

---
+++ src/ManifoldMeshes.jl
@@ -0,0 +1,12 @@
+module ManifoldMeshes
+
+using Manifolds
+using ManifoldsBase
+using StaticArrays
+using LinearAlgebra
+
+include("traits.jl")
+include("interface.jl")
+include("sphere/latlon.jl")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,10 @@
+using ManifoldMeshes
+using Test
+
+@testset "ManifoldMeshes.jl" begin
+    include("test_traits.jl")
+    include("test_latlon_construction.jl")
+    include("test_latlon_geometry.jl")
+    include("test_latlon_connectivity.jl")
+    include("test_latlon_normals.jl")
+end
